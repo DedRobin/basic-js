8b911ac9fddb162cabb42e0099f87a97978af613
Pavel Rabekin <pavelrabekin@gmail.com>
2023-12-23 12:42:48 +0300

    feat: task (repeater) is solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let array = [];
+  if ('' + options.addition === 'false' || '' + options.addition === 'null' || options.addition) {
+    let addition = options.addition;
+    if (options.additionRepeatTimes) {
+      addition = new Array(options.additionRepeatTimes).fill(options.addition);
+      if ('' + options.addition === 'null') {
+        addition = addition.map(item => '' + item);
+      }
+    }
+    if (Array.isArray(addition)) {
+      (options.additionSeparator) ? addition = addition.join(options.additionSeparator) : addition = addition.join('|');
+    }
+    str += addition;
+  }
+  if (options.repeatTimes) {
+    array = new Array(options.repeatTimes).fill(str);
+  } else {
+    array.push(str);
+  }
+  // debugger
+  if (options.separator) return array.join(options.separator);
+  return array.join('+');
 }
 
 module.exports = {
