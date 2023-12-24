34cacd3a219e24550728a8198ec423dd93aaaacd
Pavel Rabekin <pavelrabekin@gmail.com>
2023-12-24 13:58:32 +0300

    feat: task (VigenereCipheringMachine) is solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverseIt = true) {
+    this.originalString;
+    this.reverseIt = reverseIt;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  toConvertKey(key, str) {
+    key = key.repeat(Math.ceil(str.length / key.length)).slice(0, str.length);
+    const newKey = [];
+    let j = 0;
+    for (let i = 0; i < str.length; i++) {
+      const charCode = str[i].charCodeAt();
+      const condition = charCode >= 65 && charCode <= 90;
+      if (condition) {
+        newKey.push(key[j++]);
+      } else {
+        newKey.push(str[i]);
+      }
+    }
+    return newKey.join('');
+  }
+  encrypt(str = undefined, key = undefined) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    if (!this.reverseIt) str = str.split('').reverse().join('');
+
+    this.originalString = str.toUpperCase();
+
+    const chiperStr = [];
+
+    const upperKey = key.toUpperCase();
+    const upperStr = str.toUpperCase();
+    const upperConvertKey = this.toConvertKey(upperKey, upperStr);
+
+    for (let i = 0; i < upperStr.length; i++) {
+      const charCodeOfStr = upperStr[i].charCodeAt();
+      const charCodeOfKey = upperConvertKey[i].charCodeAt();
+      if (charCodeOfStr >= 65 && charCodeOfStr <= 90) {
+        const newCharCodeOfStr = (charCodeOfStr + charCodeOfKey) % 26 + 65;
+        chiperStr.push(String.fromCharCode(newCharCodeOfStr));
+      } else {
+        chiperStr.push(upperStr[i]);
+      }
+    }
+    return chiperStr.join('');
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    if (!this.reverseIt) str = str.split('').reverse().join('');
+    if (!str || !key) throw new Error('Incorrect arguments!');
+
+    const decryptedStr = [];
+
+    const upperKey = key.toUpperCase();
+    const upperStr = str.toUpperCase();
+    const upperConvertKey = this.toConvertKey(upperKey, upperStr);
+
+    for (let i = 0; i < upperStr.length; i++) {
+      const charCodeOfStr = upperStr[i].charCodeAt();
+      const charCodeOfKey = upperConvertKey[i].charCodeAt();
+      if (charCodeOfStr >= 65 && charCodeOfStr <= 90) {
+        const newCharCodeOfStr = Math.abs(charCodeOfStr - charCodeOfKey + 26) % 26 + 65;
+        decryptedStr.push(String.fromCharCode(newCharCodeOfStr));
+      } else {
+        decryptedStr.push(upperStr[i]);
+      }
+    }
+    return decryptedStr.join('');
   }
 }
 
